22ba5274314b4b4c2cb87ba24ba1b3b57745d5ee
Aleksandar Ichev <aleksandar.ichev@one-inside.com>
2023-08-31 16:00:09 +0200

    Added images of the coffee shop to enhance visual appeal.

--- src/app/components/page-one-photos/page-one-photos.component.ts
+++ src/app/components/page-one-photos/page-one-photos.component.ts
@@ -3,26 +3,48 @@ import { PageTwoLocationComponent } from '../page-two-location/page-two-location
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DataProductsService } from 'src/app/services/data-products.service';
 
 @Component({
   selector: 'app-page-one-photos',
   templateUrl: './page-one-photos.component.html',
   styleUrls: ['./page-one-photos.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule],
+  imports: [IonicModule, CommonModule, FormsModule, HttpClientModule],
+  providers: [DataProductsService],
 })
 export class PageOnePhotosComponent implements OnInit {
   component = PageTwoLocationComponent;
 
-  constructor() {}
+  public jsonData: any = [];
 
-  ngOnInit() {}
+  constructor(private dataProductsService: DataProductsService) {}
+
+  ngOnInit() {
+    this.dataFetch();
+  }
+
+  dataFetch() {
+    this.dataProductsService.getCoffeeBarPhotos();
+    this.dataProductsService.jsonData$.subscribe((data) => {
+      this.jsonData = data;
+    });
+  }
 
   ionViewWillEnter() {
     console.log('Page One View-Will-Enter');
   }
 
+  ionViewDidEnter() {
+    console.log('Page One View-Did-Enter');
+  }
+
   ionViewWillLeave() {
     console.log('Page One View-Will-Leave');
+  }
+
+  ionViewDidLeave() {
+    console.log('Page One View-Did-Leave');
   }
 }
